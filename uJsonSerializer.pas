2f8cb1dafff1584ce3217cf8e117a033713e3c1e
rachetfoot <jason@boileau.dk>
2016-11-11 14:16:44 +0100

    Add global custom marshallers

--- uJsonSerializer.pas
+++ uJsonSerializer.pas
@@ -2,43 +2,50 @@ unit uJsonSerializer;
 
 interface
 
-uses sysutils, classes, generics.collections;
+uses sysutils, classes, generics.collections, superobject, TypInfo;
 
 type
   TJsonSerializer = class
+  private
+    class var ctx: TSuperRttiContext;
+    class constructor Create;
+    class destructor Destroy;
   public
     class function Serialize<T>(const obj: T): String;
     class function Deserialize<T>(const Json: string): T;
+    class procedure RegisterCustomMarshaller(TypeInfo: PTypeInfo;
+      FromJSON: TSerialFromJson; ToJSON: TSerialToJson);
   end;
 
 implementation
 
-uses superobject;
-
 { TJsonSerializer }
 
-class function TJsonSerializer.Deserialize<T>(const Json: string): T;
-var
-  ctx: TSuperRttiContext;
+class constructor TJsonSerializer.Create;
 begin
   ctx := TSuperRttiContext.Create;
-  try
-    result := ctx.AsType<T>(SO(Json));
-  finally
-    freeandnil(ctx);
-  end;
+end;
+
+class destructor TJsonSerializer.Destroy;
+begin
+  freeandnil(ctx);
+end;
+
+class procedure TJsonSerializer.RegisterCustomMarshaller(TypeInfo: PTypeInfo;
+  FromJSON: TSerialFromJson; ToJSON: TSerialToJson);
+begin
+  ctx.SerialFromJson.Add(TypeInfo, FromJSON);
+  ctx.SerialToJson.Add(TypeInfo, ToJSON);
+end;
+
+class function TJsonSerializer.Deserialize<T>(const Json: string): T;
+begin
+  result := ctx.AsType<T>(SO(Json));
 end;
 
 class function TJsonSerializer.Serialize<T>(const obj: T): String;
-var
-  ctx: TSuperRttiContext;
 begin
-  ctx := TSuperRttiContext.Create;
-  try
-    result := ctx.AsJson(obj).AsJson(true, false);
-  finally
-    freeandnil(ctx);
-  end;
+  result := ctx.AsJson(obj).AsJson(true, false);
 end;
 
 end.
